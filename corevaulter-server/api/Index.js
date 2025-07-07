f400399002ae7510a2a558d5b7844e4d17fd7bf2
Ayomide <isaacayomide2359@gmail.com>
2025-07-07 23:25:00 +0100

    server

--- corevaulter-server/api/Index.js
+++ corevaulter-server/api/Index.js
@@ -58,9 +58,7 @@ app.use(
     max: 100,
   })
 );
-app.get("/", (req, res) => {
-  res.send("hello its working");
-});
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
